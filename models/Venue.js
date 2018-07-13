d42fe32c99eed01d432b7136aaa6cb76c4b767b5
KJax101 <keng4105@gmail.com>
2018-07-12 18:05:31 -0700

    Create Venue model

---
+++ models/Venue.js
@@ -0,0 +1,21 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var VenueSchema = new Schema({
+  name: {
+    type: String
+  },
+  icon: {
+    type: String
+  },
+  address: {
+    type: String
+  },
+  reference: {
+  	type: String
+  }
+});
+
+var Venue = mongoose.model("Venue", VenueSchema);
+module.exports = Venue;
