36388411719a3340bf692ec32f2fee254cd80fc5
KJax101 <keng4105@gmail.com>
2018-07-14 20:34:15 -0400

    using prcess.ext to complete

--- test/root-tests.js
+++ test/root-tests.js
@@ -1,50 +1,52 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const { app, runServer, closeServer } = require('../server');
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const { app, runServer, closeServer } = require("../server");
 const expect = chai.expect;
 chai.use(chaiHttp);
 
-describe('Venue Finder', function () {
-
-  before(function () {
+describe("Venue Finder", function() {
+  before(function() {
     return runServer();
   });
 
-
-  after(function () {
+  after(function() {
     return closeServer();
   });
 
-  it('executes the get `saved` route with no errors', function () {
-    return chai.request(app)
-      .get('/api/saved')
-      .then(function (res) {
+  it("executes the get `saved` route with no errors", function() {
+    return chai
+      .request(app)
+      .get("/api/saved")
+      .then(function(res) {
         expect(res).to.have.status(200);
       });
-  }); 
+  });
 
-  it('executes the post `places` route with no errors', function () {
-    return chai.request(app)
-      .post('/api/places')
-      .then(function (res) {
+  it("executes the post `places` route with no errors", function() {
+    return chai
+      .request(app)
+      .post("/api/places")
+      .then(function(res) {
         expect(res).to.have.status(200);
       });
-  }); 
+  });
 
-  it('executes the post `placeHourInfo` route with no errors', function () {
-    return chai.request(app)
-      .get('/api/saved')
-      .then(function (res) {
+  it("executes the post `placeHourInfo` route with no errors", function() {
+    return chai
+      .request(app)
+      .get("/api/saved")
+      .then(function(res) {
         expect(res).to.have.status(200);
       });
-  }); 
+  });
 
-  it('executes the post `saved` route with no errors', function () {
-    return chai.request(app)
-      .get('/api/saved')
-      .then(function (res) {
+  it("executes the post `saved` route with no errors", function() {
+    return chai
+      .request(app)
+      .get("/api/saved")
+      .then(function(res) {
         expect(res).to.have.status(200);
+        process.exit();
       });
-  }); 
-
+  });
 });
